cb88ab7fc28b2fddf679aa3623c9ddea396f0867
Denis <deny.postolachi88@gmail.com>
2022-06-13 23:03:00 +0300

    init

---
+++ app.js
@@ -0,0 +1,74 @@
+const date = new Date();
+const leftArrow = document.querySelector('.pre')
+const rightArrow = document.querySelector('.next')
+
+const createCalendar = () => {
+    date.setDate(1)
+    const months = [
+        'Jan.',
+        'Feb.',
+        'Mar.',
+        'Apr.',
+        'May.',
+        'Jun.',
+        'Jul.',
+        'Aug.',
+        'Sep.',
+        'Oct.',
+        'Nov.',
+        'Dec.',
+    ]
+    const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate()
+    const lastDayPreviousMonth = new Date(date.getFullYear(), date.getMonth(), 0).getDate()
+    const firstDayIndex = date.getDay()
+    const lastDayIndex = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDay()
+    const nextDays = 7 - lastDayIndex - 1
+
+    const dateHeading = document.querySelector('.date h1')
+    const dateText = document.querySelector('.date p')
+    const monthDays = document.querySelector('.days')
+
+
+    dateHeading.innerHTML = months[date.getMonth()]
+    dateText.innerHTML = new Date().toDateString()
+
+    let days = '';
+
+    for (let i = firstDayIndex; i > 0; i--) {
+        days += `<div class="prev-date">${lastDayPreviousMonth - i + 1}</div>`
+    }
+
+    for (let i = 1; i <= lastDay; i++) {
+        if (
+            i === new Date().getDate() &&
+            date.getMonth() === new Date().getMonth()
+        ) {
+            days += `<div class="today">${i}</div>`;
+        } else {
+            days += `<div>${i}</div>`;
+        }
+    }
+
+    for (let i = 1; i <= nextDays; i++) {
+        days += `<div class="next-date">${i}</div>`
+    }
+
+    monthDays.innerHTML = days
+}
+
+
+leftArrow.addEventListener('click', () => {
+    date.setMonth(date.getMonth() - 1)
+    createCalendar()
+})
+rightArrow.addEventListener('click', () => {
+    date.setMonth(date.getMonth() + 1)
+    createCalendar()
+})
+
+createCalendar()
+
+
+
+
+
